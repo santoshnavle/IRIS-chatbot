e19a6c18d1579ffee1a86a97db84af3567a3f965
santoshnavle <santoshnavle2010@gmail.com>
2020-09-19 23:04:30 +0530

    lots of changes as per requirement

--- js/main.js
+++ js/main.js
@@ -7,15 +7,33 @@ $(document).ready(function(){
 
          setTimeout(function(){ 
             $(".faq-q ul li").each(function(i) {
-                $(this).delay(2000 * i).fadeIn(1000);                
+                $(this).delay(2000 * i).fadeIn(1000);                               
             });         
          }, 2000);
+
+
+        setTimeout(function(){ 
+            $('.conversion-btn').fadeIn(800);           
+         }, 300);
+       
          
-         $('#close-chat').slideDown(500);
+         $('#close-chat').show(500);
     })
 
     $('#close-chat').click(function(){
         $('#chat-box').slideUp();    
         $(this).hide();
     })
+
+    if($('#chat-box').height() > 400+ 'px'){
+        $('.chatboat .chat-head').css({'max-width': '100%'});
+    }
+
+    
+
+
+    // custom scroll
+    new PerfectScrollbar('.scroll-box');
+
+     
 })
